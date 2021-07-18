9ae9c460748790340c4adc9bf8e391e15f034f95
edmobe <e.d.mobe@hotmail.com>
2021-07-17 23:46:57 -0600

    Update basic structure to make it responsive

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to(".name", 1, {
-      'font-size': "0em",
-    })
-  );
+  TweenLite.to(".name", 1, {
+    "font-size": "0px",
+  })
+);
 
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
   triggerElement: ".home",
   duration: 500,
-  triggerHook: 0
+  triggerHook: 0,
 })
   .setTween(tween)
-  .setPin('.home')
-  .addIndicators()
-  .addTo(controller);
+  .setPin(".home")
+  .addTo(controller); //.addIndicators()
