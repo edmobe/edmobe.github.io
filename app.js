2240e6f54e98abf6d684d72e73703026ee9f15ff
edmobe <e.d.mobe@hotmail.com>
2021-07-20 00:13:28 -0600

    Add work cards with parallax effect

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-/* GENERAL */   
+/* GENERAL */
 const TOGGLE_WIDTH = 1000;
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
 const controller = new ScrollMagic.Controller();
@@ -23,36 +23,61 @@ homeTween.add(
 );
 
 const aboutImagePath = {
-    values: [
-        {x: 0, y: 0}
-    ]
-}
+  values: [{ x: 0, y: 0 }],
+};
 
 const homeScene = new ScrollMagic.Scene({
-    triggerElement: ".home",
-    duration: 500,
-    triggerHook: 0,
-  })
-    .setTween(homeTween)
-    .setPin(".home")
-    .addTo(controller); //.addIndicators();
+  triggerElement: ".home",
+  duration: 500,
+  triggerHook: 0,
+})
+  .setTween(homeTween)
+  .setPin(".home")
+  .addTo(controller); //.addIndicators();
 
 /* ABOUT */
 const aboutTween = new TimelineLite();
 aboutTween.add(
-    TweenLite.to('.person-image', 1, {
-        bezier: aboutImagePath,
-        ease: Power1.easeInOut,
-    })
+  TweenLite.to(".person-image", 1, {
+    bezier: aboutImagePath,
+    ease: Power1.easeInOut,
+  })
 );
 
 const aboutScene = new ScrollMagic.Scene({
-    triggerElement: ".about",
-    duration: 420,
-    triggerHook: 0.5,
+  triggerElement: ".about",
+  duration: 420,
+  triggerHook: 0.5,
+})
+  .setTween(aboutTween)
+  .addTo(controller); //.addIndicators();
+
+/* WORK */
+VanillaTilt.init(document.querySelectorAll(".card-box"), {
+  max: 15,
+  speed: 100,
+});
+
+const workTween = new TimelineLite();
+workTween.add(
+  TweenLite.to(".card-container .card-box .card-name", 1, {
+    top: "20px",
+    opacity: "1",
   })
-    .setTween(aboutTween)
-    .addTo(controller); //.addIndicators();
+);
+workTween.add(
+    TweenLite.to(".card-container .card-box .card-read", 1, {
+        bottom: "30px",
+        opacity: "1"
+    })
+)
 
 
+const workScene = new ScrollMagic.Scene({
+  triggerElement: ".work",
+  duration: 500,
+  triggerHook: 0.5,
+})
+  .setTween(workTween)
+  .addTo(controller);
 
